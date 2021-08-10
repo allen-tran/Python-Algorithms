08e1f4dd9a8336bcf7b412d426322e5f51904b46
Allen Tran <allentran08@gmail.com>
2021-08-10 13:38:50 -0700

    Added time complexities. Bubble sort is done

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -1,19 +1,19 @@
 '''
-
+Time Complexity: O(n^2)
 
 '''
 
 def bubble_sort(sequence):
-    index_length = len(sequence) - 1
+    index_length = len(sequence) - 1 # define last index
 
-    sorted = False
-    while not sorted:
-        sorted = True
-        for i in range(0, index_length):
-            if sequence[i] > sequence[i+1]:
-                sorted = False
-                sequence[i], sequence[i+1] = sequence[i+1], sequence[i]
+    sorted = False # create sorted flag
+    while not sorted: # while its not sorted
+        sorted = True # first set sorted to true but if it enters the if statement, its not truly sorted
+        for i in range(0, index_length): # for loop going until the last index
+            if sequence[i] > sequence[i+1]: # if the current index is greater than the next index
+                sorted = False # it is not sorted
+                sequence[i], sequence[i+1] = sequence[i+1], sequence[i] # swap the values
 
-    return sequence
+    return sequence # sequence should be sorted
 
-print(bubble_sort([4,6,3,2,6,7,8,5,3,6,8]))
+print(bubble_sort([4,6,3,2,6,7,8,5,3,6,8])) # random list of numbers for testing
