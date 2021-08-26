26aa2156a9d6cf7520d9130ad5065709432f8e1c
Allen Tran <allentran08@gmail.com>
2021-08-26 14:29:01 -0700

    Added hashmap solution twosum

--- leetcode/easy/two_sum.py
+++ leetcode/easy/two_sum.py
@@ -2,6 +2,7 @@
 #1 easy
 '''
 # BRUTE FORCE
+'''
 def twoSum(nums, target: int):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
@@ -11,4 +12,22 @@ def twoSum(nums, target: int):
 
 print(twoSum([2,6,3,5,8], 5))
 
-# HASH MAP
+'''
+
+# HASH MAP aka One Pass
+# Time: O(n)
+# Space: O(n)
+
+def twoSum(nums, target):
+    # every previous element we visit will be stored in here
+    # value : index
+    prevMap = {}
+    # i id index and value is n 
+    for i, n in enumerate(nums):
+        # we need to first check to see if the difference is already in the hashmap or not
+        diff = target - n
+        #  if it is, we can return the indices first of the difference index and the current element index
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+    return
---
+++ testing/list_slice.py
@@ -0,0 +1,9 @@
+def convert(string):
+    list = []
+    list[::] = string
+    for i in list:
+        if i == 
+
+
+str1 = "ABCD"
+print(convert(str1))
