f989a862bfe10dc26bcb46c3d4f9a1ef87077662
Allen Tran <allentran08@gmail.com>
2021-08-29 08:59:56 -0700

    Added subarray sum eqaul to k leetcode

--- leetcode/medium/subarray_sum_equals.py
+++ leetcode/medium/subarray_sum_equals.py
@@ -2,3 +2,17 @@
 #560 medium
 '''
 
+def subarraySum(nums, k):
+    prevMap = {0:1}
+    count = 0
+    s = 0
+
+    for num in nums:
+        s+= num
+        if s-k in prevMap:
+            count += prevMap-[s-k]
+        if s in prevMap:
+            prevMap[s] += 1
+        else:
+            prevMap[s] = 1
+    return count
---
+++ sorting/tim_sort.py
@@ -0,0 +1,5 @@
+'''
+Hybrid algorithm that is used for sort()
+Combination of merge sort and insertion sort
+'''
+
