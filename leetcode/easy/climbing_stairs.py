d3f0ab0832b46fedc9f8b68bb1300003a0c835a2
Allen Tran <allentran08@gmail.com>
2021-08-30 08:52:02 -0700

    Climbing stairs annotations

--- leetcode/easy/climbing_stairs.py
+++ leetcode/easy/climbing_stairs.py
@@ -1,18 +1,26 @@
 '''
 #70 easy
 Time: O(n)
-- similar to fibonacci series
+- uses fibonacci sequence
 '''
 
 def climb(n):
+    # the first two stairs will only have either one or two distinct ways to get up
     if n <= 2:
+        # just return whatever n is if its 2 or less
         return n
     else:
+        # if not, we have a be 1 and b be the second way to get up
         a, b = 1, 2
+        # we dont go all the way up because the first two are already taken care of
         for i in range(n-2):
+            # first add the two numbers together
             c = a + b
+            # then we shift to the right. make a the second largest number
             a = b
+            # make b the largest number (newest number)
             b = c
+    # we return b because it will always be the right most, greatsest number        
     return b
 
 print(climb(5))
