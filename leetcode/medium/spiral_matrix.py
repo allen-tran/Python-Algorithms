ffac52b2dd4676b649ec13c43c041c37b1edd41e
Allen Tran <allentran08@gmail.com>
2021-09-01 07:11:24 -0700

    Added spiral matrix

--- leetcode/medium/spiral_matrix.py
+++ leetcode/medium/spiral_matrix.py
@@ -1,13 +1,42 @@
 '''
 #54 medium
+Time Complexity: O(m*n)
+Space Complexity: O(1)
 '''
 
 def spiral(matrix):
     if not matrix:
         return []
     
-    rowbegin = 0
-    rowend = len(matrix)
-    columnbegin = 0
-    columnend = len(matrix)
+    res = [] # result
+    left, right = 0, len(matrix[0])
+    top, bottom = 0, len(matrix)
+
+    while left < right and top < bottom:
+        # get every value in the top row
+        for i in range(left, right): # from 0-2 (3)
+            res.append(matrix[top][i])
+        top += 1 # it will shift top bound by 1
+
+        # get every i in the right col
+        for j in range(top, bottom):
+            res.append(matrix[j][right - 1])
+        right -= 1 # it will shift right bound over to the left by 1
+
+        if not (left < right and top < bottom):
+            break
+
+        # get every i in the bottom row
+        for k in range(right - 1, left - 1, -1):
+            res.append(matrix[bottom - 1][k])
+        bottom -= 1 # it will shift bottom bound up to by 1
+
+        # get every i in the left column
+        for l in range(bottom - 1, top -1, -1):
+            res.append(matrix[l][left])
+        left += 1
+    
+    return res
+        
+print(spiral([[1,2,3], [4, 5, 6], [7, 8, 9]]))
     
