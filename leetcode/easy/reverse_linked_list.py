f45d18e60cf7c04434593ad1a9e82bb57fb7fad0
Allen Tran <allentran08@gmail.com>
2021-09-02 19:06:29 -0700

    Reverse linked list -> no annotations

---
+++ leetcode/easy/reverse_linked_list.py
@@ -0,0 +1,14 @@
+'''
+#206 easy
+'''
+
+def reverse(head):
+    prev = None
+      
+    while (head):
+        temp = head
+        head = head.next
+        temp.next = prev
+        prev = temp
+            
+    return prev
