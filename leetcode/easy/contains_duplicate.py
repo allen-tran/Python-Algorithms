d4bf823aa41867465088c71cccdb0fe7ad018b7d
Allen Tran <allentran08@gmail.com>
2021-09-07 09:32:51 -0700

    Added two easy LC's

---
+++ leetcode/easy/contains_duplicate.py
@@ -0,0 +1,16 @@
+'''
+#217 easy
+'''
+
+def containsDuplicate(self, nums: List[int]) -> bool:
+    dict = {}
+        
+    for i in range(len(nums)):
+        if nums[i] not in dict:
+            dict[nums[i]] = [i]
+        else:
+            dict[nums[i]].append(i)
+        if len(dict[nums[i]]) >= 2:
+            return True
+            
+    return False
---
+++ leetcode/easy/missing_number.py
@@ -0,0 +1,8 @@
+'''
+#268 easy
+'''
+
+def missingNumber(self, nums: List[int]) -> int:
+    for i in range(len(nums)+1):
+        if i not in nums:
+            return i
