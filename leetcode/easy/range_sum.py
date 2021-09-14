bf580759f15f97649655d9fbb777da5744029bcc
Allen Tran <allentran08@gmail.com>
2021-09-14 12:52:10 -0700

    Added range sum query

---
+++ leetcode/easy/range_sum.py
@@ -0,0 +1,18 @@
+class NumArray:
+    dict = {}
+    def __init__(self, nums):
+        
+        self.dict[0] = nums[0]
+        for i in range(1, len(nums)):
+            self.dict[i] = self.dict[i-1] + nums[i]
+
+    def sumRange(self, left: int, right: int):
+        if left == 0:
+            return self.dict[right]
+        else:
+            return (self.dict[right] - self.dict[left-1])
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
