cbeaa9f49d8727fe4ae62bf1beb65453082c4c25
Allen Tran <allentran08@gmail.com>
2021-09-18 14:36:22 -0700

    Implement strStr()

---
+++ leetcode/easy/strStr.py
@@ -0,0 +1,9 @@
+def strStr(self, haystack: str, needle: str) -> int:
+    if not needle:
+        return 0
+        
+    for i in range(0, len(haystack)):
+        temp = str(haystack[i:len(needle)+i])
+        if temp == needle:
+            return i
+    return -1
