4e3ccf77411daee2d59af206649e8db1301a944c
Allen Tran <allentran08@gmail.com>
2021-09-19 07:59:14 -0700

    Symmetric

---
+++ leetcode/easy/symmetric_tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSymmetric(self, root):
+        if root is None:
+            return
+        return self.isMirror(root, root)
+    
+    
+    def isMirror(self, left, right):
+        if left == None and right == None:
+            return True
+        elif left == None or right == None:
+            return False
+        else:
+            return left.val == right.val and self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left)
+            
