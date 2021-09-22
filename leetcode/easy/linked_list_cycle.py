80086bbf938311e99604e0fc9c0f3267ded531cc
Allen Tran <allentran08@gmail.com>
2021-09-21 20:40:42 -0700

    linked list cycle

---
+++ leetcode/easy/linked_list_cycle.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def hasCycle(head: ListNode):
+    
+    slow, fast = head, head
+        
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if fast == slow:
+            return True
+            
+    return False
