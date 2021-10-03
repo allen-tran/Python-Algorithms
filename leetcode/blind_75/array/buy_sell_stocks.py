cee0238376e5bf73c8b1bf24da7462c31470b125
Allen Tran <allentran08@gmail.com>
2021-10-03 09:31:14 -0700

    implementd blind 75

---
+++ leetcode/blind_75/array/buy_sell_stocks.py
@@ -0,0 +1,24 @@
+'''
+#121 easy
+'''
+
+def buy_sell(prices):
+    # create pointers to the stock prices
+    l, r =  0, 1
+    # create max variable to compare to
+    maxP = 0
+
+    # while right pointer doesn't reach out of bounds
+    while r < len(prices):
+        # left value should never be greater than the right value because we only want profits
+        if prices[l] < prices[r]:
+            # compare the profit
+            profit = prices[r] - prices[l]
+            maxP = max(maxP, profit)
+        else:
+            # if we found a value smaller than left, we want to update left to that value
+            l = r
+        # keep incrementing no matter what
+        r+=1
+    
+    return maxP
