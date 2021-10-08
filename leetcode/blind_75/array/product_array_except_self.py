b551fc144aaa9fd5678833e137ce70e2d3cf9656
Allen Tran <allentran08@gmail.com>
2021-10-08 14:01:36 -0700

    two sum ii

--- leetcode/blind_75/array/product_array_except_self.py
+++ leetcode/blind_75/array/product_array_except_self.py
@@ -4,10 +4,12 @@ def productExceptSelf(nums):
     # initialize pre and post fix lists within value 1 to start off the multiplying
     prefix = [1]
     postfix = [1]
-        
+
+    # multiply the number in prefix with the numbers    
     for i in range(len(nums)):
         prefix.append(prefix[i]* nums[i])
         
+    # mutliply going down the lists
     for i in range(len(nums)):
         postfix.insert(0, (postfix[i-(i+i+1)] * nums[i-(i+i+1)]))
             
