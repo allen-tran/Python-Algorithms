53da64030261bde5adc39acbc062fde8e9806ff4
Allen Tran <allentran08@gmail.com>
2021-10-09 15:19:48 -0700

    water

---
+++ leetcode/medium/container_most_water.py
@@ -0,0 +1,13 @@
+def maxArea(self, height: List[int]) -> int:
+    l, r = 0, len(height)-1
+    currArea = 0
+    while l < r:
+        newArea = (r-l) * min(height[l], height[r])
+        currArea = max(currArea, newArea)
+        if height[l] < height[r]:
+            l+=1
+        elif height[l] > height[r]:
+            r-=1
+        else:
+            l += 1
+    return currArea
