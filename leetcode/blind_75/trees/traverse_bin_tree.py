7965e7622cf7a4a12e4ff872be5ca62ee6e4ffd6
Allen Tran <allentran08@gmail.com>
2021-10-11 19:50:06 -0700

    traverse bin tree

---
+++ leetcode/blind_75/trees/traverse_bin_tree.py
@@ -0,0 +1,23 @@
+def levelOrder(root):
+    if not root:
+        return None
+        
+    q = deque()
+    q.append(root)
+    res = []
+    while q:
+        new_list = []
+        qsize = len(q)
+        for i in range(qsize):
+            rem = q.popleft()
+            if rem.left:
+                q.append(rem.left)
+
+            if rem.right:
+                q.append(rem.right)
+            new_list.append(rem.val)
+        res.append(new_list)
+            
+                
+        
+    return res
