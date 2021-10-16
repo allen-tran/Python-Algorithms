cbf26f51a90444a18464ff8aa7abbd5957b54646
Allen Tran <allentran08@gmail.com>
2021-10-16 08:05:16 -0700

    longest substring

---
+++ leetcode/blind_75/string/longest_sub_without_repeat.py
@@ -0,0 +1,23 @@
+def lengthOfLongestSubstring(s):
+    # create new set
+    newSet = set()
+    # create left point
+    l = 0
+    # resultant length
+    res = 0
+    
+    # loop through string with right pointer as iterator
+    for r in range(len(s)):
+        # if we found a repeating chracter
+        while s[r] in newSet:
+            # remove the first occurance
+            newSet.remove(s[l])
+            # advance left pointer
+            l+=1
+        # add the new chacter to set no matter what
+        newSet.add(s[r])
+        # find out the max by comparing the right pointer minus the left pointer 
+        # but plus 1 to get the true lenghth of the substring
+        res = max(res, (r - l) + 1)
+        
+    return res
