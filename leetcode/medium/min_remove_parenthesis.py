99a279ea2db0686dd3a7e99906bacf76fd1ee8ce
Allen Tran <allentran08@gmail.com>
2021-10-16 08:17:59 -0700

    min remove parenthesis

---
+++ leetcode/medium/min_remove_parenthesis.py
@@ -0,0 +1,18 @@
+ def minRemoveToMakeValid(self, s):
+    parenthese = []
+    invalid_index = []
+    for i, c in enumerate(s):
+        if c == '(':
+            parenthese.append(c)
+            invalid_index.append(i)
+        if c == ')':
+            if len(parenthese) == 0:
+                invalid_index.append(i)
+            else:
+                parenthese.pop()
+                invalid_index.pop()        
+    res = ''
+    for j in range(len(s)):
+        if j not in invalid_index:
+            res += s[j]
+    return res
