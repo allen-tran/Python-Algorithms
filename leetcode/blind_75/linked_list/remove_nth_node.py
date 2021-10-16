9ec1e5a3358d9a1ee58b70bb642f35b12954e87a
Allen Tran <allentran08@gmail.com>
2021-10-16 16:20:37 -0700

    remove nth node from end

---
+++ leetcode/blind_75/linked_list/remove_nth_node.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def removeNthFromEnd(head, n):
+    if not head:
+        return []
+    dummy = ListNode(None)
+    dummy.next = head
+    slow = dummy
+    fast = dummy
+    
+    for i in range(n+1):
+        fast = fast.next
+    while fast:
+        slow = slow.next
+        fast = fast.next
+
+    slow.next = slow.next.next
+    return dummy.next
