5866695c3caf15cabdf64edfd1e37bcccad9ff7d
Allen Tran <allentran08@gmail.com>
2021-10-17 20:36:02 -0700

    course schedule

---
+++ leetcode/blind_75/graphs/course_schedule.py
@@ -0,0 +1,26 @@
+def canFinish(numCourses, prerequisites):
+    preMap = {i:[] for i in range(numCourses)}
+    
+    for crs, pre in prerequisites:
+        preMap[crs].append(pre)
+    
+    visitSet = set()
+    
+    def dfs(crs):
+        if crs in visitSet:
+            return False
+        if preMap[crs] == []:
+            return True
+        
+        visitSet.add(crs)
+        for i in preMap[crs]:
+            if not dfs(i):
+                return False
+        visitSet.remove(crs)
+        preMap[crs] = []
+        return True
+        
+    for crs in range(numCourses):
+        if not dfs(crs):
+            return False
+    return True
