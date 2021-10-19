068e26d64bed6c8e1528575d104bbb01d9ebfe93
Allen Tran <allentran08@gmail.com>
2021-10-19 13:19:32 -0700

    valid palindrome

---
+++ leetcode/blind_75/string/valid_palindrome.py
@@ -0,0 +1,9 @@
+def isPalindrome(s):
+    res = []
+    for i in s:
+        if i.isalpha() or i.isnumeric():
+            res.append(i.lower())
+    if res == res[::-1]:
+        return True
+    else:
+        return False
