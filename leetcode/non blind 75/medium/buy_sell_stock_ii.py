24e221351535d6c3713266274a98040740700776
Allen Tran <allentran08@gmail.com>
2021-11-15 21:37:30 -0800

    added

---
+++ leetcode/non blind 75/medium/buy_sell_stock_ii.py
@@ -0,0 +1,18 @@
+'''
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+'''
+
+
+def maxProfit(prices):
+    # iterate throgh the for loop
+    # only concern is how to know whne to buy it again and sell it
+    i = 0
+    result = 0
+
+    while i < len(prices)-1:
+        curr = i + 1
+        if prices[curr] - prices[i] > 0:
+            result += prices[curr] - prices[i]
+        i = curr
+
+    return result
