742d9fe9d56c1c8e4a8145ea9ada20c87e7a7aa5
Allen Tran <allentran08@gmail.com>
2021-11-17 10:38:47 -0800

    binary search

---
+++ leetcode/non blind 75/array/binary_search.py
@@ -0,0 +1,22 @@
+'''
+https://leetcode.com/problems/binary-search/
+'''
+
+
+def search(nums, target):
+    start, end = 0, len(nums)-1
+
+    while start <= end:
+        mid = start + (end-start) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        elif nums[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return -1
+
+
+print(search([1, 2, 3, 4, 5], 5))
