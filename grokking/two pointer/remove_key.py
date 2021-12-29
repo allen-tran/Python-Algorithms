8e01697b0110c23fad7e3de6be3714459d02bd62
Allen Tran <allentran@allens-macbook.local>
2021-12-28 19:52:10 -0800

    added remove key

--- grokking/two pointer/remove_key.py
+++ grokking/two pointer/remove_key.py
@@ -1,3 +1,13 @@
 """
-Given an unsorted array of numbers and a target ‘key’, remove all instances of ‘key’ in-place and return the new length of the array.
+Given an unsorted array of numbers and a target key, remove all instances of key in-place and return the new length of the array.
 """
+
+
+def remove_element(arr, key):
+    next_elem = 0
+
+    for i in range(len(arr)):
+        if arr[i] != key:
+            arr[next_elem] = arr[i]
+            next_elem += 1
+        return next_elem
