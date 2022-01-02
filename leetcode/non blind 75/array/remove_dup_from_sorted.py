36a25392cde1df9113193e645be8ebaeece16257
Allen Tran <allentran08@gmail.com>
2022-01-01 23:21:07 -0800

    removed duplicate

---
+++ leetcode/non blind 75/array/remove_dup_from_sorted.py
@@ -0,0 +1,21 @@
+'''
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
+'''
+
+
+def removeDuplicates(nums):
+    # start with a the next number after first
+    len_ = 1
+    # trivial case
+    if len(nums) == 0:
+        return 0
+    # starting with the second number go through
+    for i in range(1, len(nums)):
+        # check if the number before is NOT equal to what we are looking at
+        if nums[i] != nums[i-1]:
+            # if not equal we found a good number and we can set it to that
+            nums[len_] = nums[i]
+            # increase our worker
+            len_ += 1
+    # return where the work is. this how long our non duplicated array is
+    return len_
---
+++ testing/mock.py
@@ -0,0 +1,25 @@
+class Dog:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_age(self, age):
+        self.age = age
+
+class GermanShepard(Dog):
+    def __init__(self, name, age, hair_color, teeth_shape):
+        super().__init__(name, age)
+        self.hair_color = hair_color
+        self.teeth_shape = teeth_shape
+if __name__ == "__main__":
+    dog1 = GermanShepard("Tim", 34, "black", "canines")
+    print(dog1.get_name(), dog1.get_age(), dog1.hair_color, dog1.teeth_shape)
