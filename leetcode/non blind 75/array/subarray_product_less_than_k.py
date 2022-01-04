5af61297a0d669fcb4d903e00213e1959d90acf7
allen-tran <allentran08@gmail.com>
2022-01-04 09:49:03 -0800

    subarray product less than k

---
+++ leetcode/non blind 75/array/subarray_product_less_than_k.py
@@ -0,0 +1,18 @@
+'''
+https://leetcode.com/problems/subarray-product-less-than-k/
+'''
+
+
+def numSubarrayProductLessThanK(nums, k):
+    left, count, prod = 0, 0, 1
+
+    for right in range(len(nums)):
+        prod *= nums[right]
+
+        while prod >= k and left <= right:
+            prod /= nums[left]
+            left += 1
+
+        count += right - left + 1
+
+    return count
