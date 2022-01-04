eb04f549ca0f38dd78a37b05e8315c619145fcec
allen-tran <allentran08@gmail.com>
2022-01-03 20:12:21 -0800

    3 sum smaller

---
+++ leetcode/non blind 75/array/3sum_smaller.py
@@ -0,0 +1,24 @@
+'''
+https://leetcode.com/problems/3sum-smaller/
+'''
+
+
+class Solution:
+    def threeSumSmaller(self, nums, target):
+        nums.sort()
+        count = 0
+        for i in range(len(nums) - 2):
+            count += self.search_pair(nums, target-nums[i], i)
+        return count
+
+    def search_pair(self, arr, target_sum, first):
+        count = 0
+        left, right = first+1, len(arr)-1
+
+        while left < right:
+            if arr[left] + arr[right] < target_sum:
+                count += right - left
+                left += 1
+            else:
+                right -= 1
+        return count
